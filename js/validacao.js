0e792fafb118210af9d5847915131f0a827a3acd
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2022-11-24 19:39:03 -0300

    adicionando calculo do cpf

--- js/validacao.js
+++ js/validacao.js
@@ -34,12 +34,12 @@ const mensagensDeErro = {
         patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
     },
     dataNascimento: {
-        valueMissing: 'O campo cpf não pode estar vazio.',
-        customError: 'O CPF digitado não é válido.'
-    },
-    cpf: {
         valueMissing: 'O campo de data de nascimento não pode estar vazio.',
         customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        customError: 'O CPF digitado não é válido.'
     }
 }
 
@@ -77,37 +77,70 @@ function maiorQue18(data) {
     return dataMais18 <= dataAtual
 }
 
-function validaCPF(input){
-    const cpfFormatado = input.value.replace(/\D/g, ' ')
-    let mensagem = ' '
+function validaCPF(input) {
+    const cpfFormatado = input.value.replace(/\D/g, '')
+    let mensagem = ''
 
-    if(!      checaCPFRepetido(cpfFormatado)){
-        mensagem = 'O CPF digitado não é válido'
+    if (!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)) {
+        mensagem = 'O CPF digitado não é válido.'
     }
 
     input.setCustomValidity(mensagem)
 }
 
-function checaCPFRepetido(cpf){
+function checaCPFRepetido(cpf) {
     const valoresRepetidos = [
-        '000000000',
-        '111111111',
-        '222222222',
-        '333333333',
-        '444444444',
-        '555555555',
-        '666666666',
-        '777777777',
-        '888888888',
-        '999999999']
-
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
     let cpfValido = true
 
-    valoresRepetidos.forEach(valor =>{
-        if(valor === cpf){
+    valoresRepetidos.forEach(valor => {
+        if (valor == cpf) {
             cpfValido = false
         }
     })
 
     return cpfValido
 }
+
+function checaEstruturaCPF(cpf) {
+    const multiplicador = 10
+
+    return checaDigitoVerificador(cpf, multiplicador)
+}
+
+function checaDigitoVerificador(cpf, multiplicador) {
+    if (multiplicador >= 12) {
+        return true
+    }
+
+    let multiplicadorInicial = multiplicador
+    let soma = 0
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('')
+    const digitoVerificador = cpf.charAt(multiplicador - 1)
+    for (let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--) {
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+        contador++
+    }
+
+    if (digitoVerificador == confirmaDigito(soma)) {
+        return checaDigitoVerificador(cpf, multiplicador + 1)
+    }
+
+    return false
+}
+
+function confirmaDigito(soma) {
+    return 11 - (soma % 11)
+}
+
+
